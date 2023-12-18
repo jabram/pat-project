a7b821e40e87cc6294a7570117266c9e2b35d06f
jaime <jabram617@gmail.com>
2023-12-18 18:45:49 -0500

    Animate Header visibility
    - if there's no chapterTitle, hide the header (like the Title
    component)
    - when scrolling up hide the header, show header when scrolling down

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { AnimatePresence, motion } from "framer-motion";
 import ButtonUnstyled from "../ButtonUnstyled/ButtonUnstyled";
 import styles from "./Header.module.scss";
 import { ReactComponent as Hamburger } from "../../icons/hamburger.svg";
 
+// inspired by https://codepen.io/codemzy/pen/vYWjzNY?editors=1010
+function useScrollDirection() {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    let lastScrollY = window.pageYOffset;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? "down" : "up";
+      if (
+        direction !== scrollDirection &&
+        (scrollY - lastScrollY > 5 || scrollY - lastScrollY < -5)
+      ) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection);
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection);
+    };
+  }, [scrollDirection]);
+
+  return scrollDirection;
+}
+
 const Header = ({ showMenu, toggleMenu, chapterTitle }) => {
+  const scrollDirection = useScrollDirection();
+
+  const getAnimation = () => {
+    if (!chapterTitle) {
+      return "hidden";
+    }
+    if (scrollDirection === "down") {
+      return "visible";
+    } else {
+      return "hidden";
+    }
+  };
+
   return (
-    <div className={styles.header}>
-      <p className={styles.title}>
-        Sad Celebrations
-        <br />
-        and Laughable Laments <span>A Lyrical Autobiography</span>
-      </p>
+    <AnimatePresence>
+      <motion.div
+        className={styles.header}
+        initial={chapterTitle ? "visible" : "hidden"}
+        animate={getAnimation()}
+        variants={{
+          visible: { top: 0 },
+          hidden: { top: "-100px" },
+        }}
+      >
+        <p className={styles.title}>
+          Sad Celebrations
+          <br />
+          and Laughable Laments <span>A Lyrical Autobiography</span>
+        </p>
 
-      <p className={styles.chapterTitle}>{chapterTitle}</p>
+        <p className={styles.chapterTitle}>{chapterTitle}</p>
 
-      {showMenu && (
-        <ButtonUnstyled className={styles.menuButton} onClick={toggleMenu}>
-          <Hamburger className={styles.menuIcon} />
-        </ButtonUnstyled>
-      )}
-    </div>
+        {showMenu && (
+          <ButtonUnstyled className={styles.menuButton} onClick={toggleMenu}>
+            <Hamburger className={styles.menuIcon} />
+          </ButtonUnstyled>
+        )}
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
