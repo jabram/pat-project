a6e34e34eb0d88e45fa345fce48695f6410dd520
jaime <jabram617@gmail.com>
2023-12-27 15:18:32 -0500

    Change Header scroll behavior
    This makes the header visible when scrolling up and hides when
    scrolling down, reverse of how I had it.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -39,7 +39,7 @@ const Header = ({ showMenu, toggleMenu, chapterTitle }) => {
     if (!chapterTitle) {
       return "hidden";
     }
-    if (scrollDirection === "down") {
+    if (scrollDirection === "up") {
       return "visible";
     } else {
       return "hidden";
