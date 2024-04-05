5f1eacdd0b0fdb286ac889c603b2d40d12ce5e74
jaime <jabram617@gmail.com>
2024-04-05 15:01:29 -0400

    Allow markdown in captions

--- src/components/Caption/Caption.js
+++ src/components/Caption/Caption.js
@@ -1,13 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Caption.module.scss";
+import Markdown from "markdown-to-jsx";
 
 const Caption = ({ text }) => {
-  return <p className={styles.caption}>{text}</p>;
+  return (
+    <p className={styles.caption}>
+      <Markdown>{text}</Markdown>
+    </p>
+  );
 };
 
 Caption.propTypes = {
-  text: PropTypes.string,
+  text: PropTypes.node, // allow strings with markup
 };
 
 Caption.defaultProps = {
